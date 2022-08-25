50408dcc9379e64c1cb0fd542d931a858848a814
Louis_0723 <lursun@cybavo.com>
2022-08-25 13:00:23 +0800

    toolong

--- index.rb
+++ index.rb
@@ -7,12 +7,21 @@ if !Dir.exist?("./public")
     Dir.mkdir("./public")
 end
 
+
+toolong=["（含文獻 Reference，原文標題：The Key Elements of Gamification in Corporate Training - The Delphi Method）"]
+
 xml.xpath("//url/loc").each do |url|
     afuurl = url.text
     page_number= /\d+$/.match(afuurl)
     page = Nokogiri::HTML(URI.open( afuurl ))
     title = page.xpath("//title").text.gsub("/","+")
     body = page.xpath("//div[@class='the_content_wrapper']")
+
+    toolong.each do |str|
+        if title.include? str
+            title = title.sub(str,"")
+        end
+    end
 
     categories = page.xpath("//ul[@class='post-categories']/li").each do |categorie|
         dirname=categorie.text.gsub("/","+")
