62114de327995c4d8976dee987d4f860c37b7e73
Louis_0723 <lursun@cybavo.com>
2022-08-26 15:26:30 +0800

    fix

--- http-server.rb
+++ http-server.rb
@@ -11,21 +11,17 @@ toolong=[
 
 def decode(path)
     xml = Nokogiri::HTML(URI.open(@httpserverUrl+path))
+    puts CGI.unescape(path)
     if !Dir.exist?("./public"+CGI.unescape(path))
         Dir.mkdir("./public"+CGI.unescape(path))
     end
     
-    File.write("./public#{CGI.unescape(path)}index.html", xml.to_html)
+    File.write("./public#{CGI.unescape(path)}/index.html", xml.to_html)
     
-    xml.xpath("//tr").each do |tr|
-        trxml = Nokogiri::HTML(tr.to_html)
-        code = trxml.xpath("//td[@class='perms']/code").text
-        if code.include?("(dr")
-            pathdeep = trxml.xpath("//td[@class='display-name']/a")[0]["href"]
-            if (!pathdeep.include?(".."))
-                puts CGI.unescape(pathdeep)
-                decode(pathdeep)
-            end
+    xml.xpath("//a[contains(@class,'icon-directory')]").each do |a|
+        pathdeep = a["href"]
+        if (!a["title"].include?(".."))
+            decode(pathdeep)
         end
     end
 end
